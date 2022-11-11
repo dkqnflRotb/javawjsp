f2a09c176df9a323ba75b43f051ff16e69b6a805
rlaehdaud42 <rlaehdaud42@naver.com>
2022-11-11 17:24:57 +0900

    2022-11-11 17:24 JSP초기파일

---
+++ src/main/java/j1111/Test2.java
@@ -0,0 +1,20 @@
+package j1111;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/T2")
+public class Test2 extends HttpServlet {
+//	private static final long serialVersionUID = 1L;  //물흐르듯이 직렬방식으로 쓰기위해 씀
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		doGet(request, response);
+//	}
+}
